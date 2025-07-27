ef25573c807a87ebf5b0d64d54fb9b1043ae3bd3
chidu07 <chidanandapavan76@gmail.com>
2025-07-27 12:20:17 +0530

    day 4 code 1

---
+++ arrays/FindfrequencyOfNumber.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindfrequencyOfNumber {
+    public static void main(String[] args) {
+        int a[] = { 1, 3, 5, 6, 3, 2, 6, 3 };
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < a.length; i++) {
+            if (map.containsKey(a[i])) {
+                map.put(a[i], map.get(a[i]) + 1);
+            } else {
+                map.put(a[i], 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+    }
+
+}
