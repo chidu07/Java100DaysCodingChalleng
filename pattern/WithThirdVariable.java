bc916c50f8c2b85665e196b17fb9ba582d20c440
chidu07 <chidanandapavan76@gmail.com>
2025-08-01 19:38:00 +0530

    Day8 code 3

---
+++ pattern/WithThirdVariable.java
@@ -0,0 +1,21 @@
+package pattern;
+
+public class WithThirdVariable {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            int k = n + 1 - i;
+            // for(int j=1;j<=n-i;j++){
+            // System.out.print(" "+" ");
+            // }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k + " ");
+                k++;
+            }
+            // for (int j = i - 1; j >= 1; j--) {
+            // System.out.print(j + " ");
+            // }
+            System.out.println();
+        }
+    }
+}
