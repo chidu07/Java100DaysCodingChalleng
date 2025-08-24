84fbad42357848cb2982eb4873d1e09133495802
chidu07 <chidanandapavan76@gmail.com>
2025-08-24 18:34:12 +0530

    Day 28 code 1

---
+++ strings/StrintToInteherConvertion.java
@@ -0,0 +1,20 @@
+
+
+public class StrintToInteherConvertion {
+
+	public static void main(String[] args) {
+		String s1="23";
+		System.out.println(Integer.parseInt(s1));
+		System.out.println(Integer.valueOf(s1));
+		int n=20;
+		System.out.println(Integer.toString(n));
+		System.out.println(String.valueOf(n));
+		
+		
+
+
+
+	}
+
+}
+
