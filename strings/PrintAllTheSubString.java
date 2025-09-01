6ebf6ff67304676ac725260bce621a93dbac168a
chidu07 <chidanandapavan76@gmail.com>
2025-09-01 13:05:08 +0530

    Day 34 code 1

---
+++ strings/PrintAllTheSubString.java
@@ -0,0 +1,15 @@
+
+
+public class PrintAllTheSubString {
+
+	public static void main(String[] args) {
+		String s="Hello";
+		for(int i=0;i<s.length();i++) {
+			for(int j=i;j<s.length();j++) {
+				System.out.println(s.substring(i,j+1));
+			}
+		}
+
+	}
+
+}
